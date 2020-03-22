5b321c277d6afe96534df979fb55d071cb32ab30
elias al3x <cod3er4@gmail.com>
2020-03-22 06:41:12 -0300

    Filling

--- src/index.js
+++ src/index.js
@@ -3,17 +3,25 @@ const resultElement = document.querySelector('div#result-search ul')
 
 const data = ["Angola", "Brazil", "EUA", "Rússia"]
 
-const handleSearch = () => {
-  var val = searchElement.value 
-  if(!val) return
+const handle_insert_element = (index) => {
+  const word = data[index]
 
   let item = document.createElement('li')
-  let no = document.createTextNode(val)
+  let no = document.createTextNode(word)
   item.appendChild(no)
-  
+
   resultElement.appendChild(item)
 
   searchElement.value = ""
 }
 
+const handleSearch = () => {
+  var val = searchElement.value 
+  if(!val) return
+
+  const index = data.indexOf(val)
+  if(index !== -1) {
+    handle_insert_element(index)
+  }
+}
 searchElement.addEventListener('change', handleSearch)
